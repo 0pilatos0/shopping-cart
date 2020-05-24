d2740e206348eda33a962188a3edf6473a0bf48e
Paul <Paulvanderlei@hotmail.nl>
2020-05-24 17:33:55 +0200

    Outputting Product Data

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,107 +1,24 @@
 @extends('layouts.master')
 @section('title')
-    Laravel shopping cart
+  Laravel shopping cart
 @endsection
 @section('content')
-<div class="row">
-        
+  @foreach($products->chunk(3) as $productChunk)
+  <div class="row">
+        @foreach($productChunk as $product)
         <div class="col-xs-18 col-sm-6 col-md-4">
           <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
+            <img src="{{ $product->imagePath }}" alt="">
               <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                <h4>{{ $product->title }}</h4>
+                <p class="description">{{ $product->description }}</p>
                 <p>
-                    <div ><h5 class="price">13$</h5></div>
+                    <div ><h5 class="price">{{ $product->price }}$</h5></div>
                     <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
             </div>
           </div>
         </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-18 col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
-              <div class="caption">
-                <h4>Product Title</h4>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
-                <p>
-                    <div ><h5 class="price">13$</h5></div>
-                    <a href="#" class="btn btn-success" role="button">Add to Cart</a></p>
-            </div>
-          </div>
-        </div>
-        
-
+        @endforeach
       </div>
+  @endforeach
 @endsection
