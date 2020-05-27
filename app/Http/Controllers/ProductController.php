b9877a362a1c2e5dcf1afe34cabae9c2b1d1c0cd
Paul <Paulvanderlei@hotmail.nl>
2020-05-27 12:11:38 +0200

    Order saving

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Order;
 use App\Cart;
+use Auth;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -48,6 +50,13 @@ class ProductController extends Controller
         }
         $oldCart = session()->get('cart');
         $cart = new Cart($oldCart);
+
+        $order = new Order();
+        $order->cart = serialize($cart);
+        $order->address = $request->input('address');
+        $order->name = $request->input('name');
+
+        Auth::user()->orders()->save($order);
 
         Session::forget('cart');
         return redirect()->route('product.index')->with('success', 'Successfully purchased products');
