d8978e94cc2fd9ab026a28e338c7a5333cb227e1
Paul <Paulvanderlei@hotmail.nl>
2020-06-13 22:50:09 +0200

    Restyling checkout

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -3,54 +3,195 @@
   Laravel shopping cart
 @endsection
 @section('content')
-<div class="row">
-    <div class="col-xs-20 col-sm-6 col-md-4">
-        <h1>Checkout</h1>
-        <h4>Your Total: ${{ $total }}</h4>
-        <form action="{{ route('checkout') }}" method="post" id="checkout-form">
+<div class="container">
+      <div class="py-5 text-center">
+
+        <h2>Checkout</h2>
+        <p class="lead">
+        voor een totaal aan <strong> {{ $total }} euro</strong>
+        </p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-md-8 order-md-1">
+          <h4 class="mb-3">Billing address</h4>
+          <form class="needs-validation" action="{{ route('checkout') }}" method="post" id="checkout-form">
             <div class="row">
-                <div class="form-group ">
-                    <label for="name">Name</label> 
-                    <input id="name" name="name" type="text" required="required" class="form-control">
+              <div class="col-md-6 mb-3">
+                <label for="firstName">First name</label>
+                <input
+                  id="name" 
+                  name="name" 
+                  type="text" 
+                  
+                  class="form-control"
+                  placeholder=""
+                  required=""
+                />
+                <div class="invalid-feedback">
+                  Valid first name is required.
                 </div>
-                <div class="form-group">
-                    <label for="email">E-Mail voor bevestiging</label> 
-                    <input id="email" name="email" type="email" required="required" class="form-control">
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="lastName">Last name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="lastName"
+                  placeholder=""
+                  required=""
+                />
+                <div class="invalid-feedback">
+                  Valid last name is required.
                 </div>
-                <div class="form-group">
-                    <label for="address">Address</label> 
-                    <input id="address" name="address" type="text" required="required" class="form-control">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="card-name">Cart Holder Name</label> 
-                        <input id="card-name" name="card-name" type="text" required="required" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="card-number">Credit Card Number</label> 
-                        <input id="card-number" name="card-number" type="text" required="required" class="form-control">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="card-expiry-month">Expiration month</label> 
-                        <input id="card-expiry-month" name="card-expiry-month" type="text" required="required" class="form-control">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="card-expiry-year">Expiration Year</label> 
-                        <input id="card-expiry-year" name="card-expiry-year" type="text" required="required" class="form-control">
-                    </div>
-                    <div class="form-group col-md-2">
-                    <label for="card-cvc">CVC</label> 
-                    <input id="card-cvc" name="card-cvc" type="text" required="required" class="form-control">
-                </div> 
-                </div>
-                
+              </div>
             </div>
+
+            <div class="mb-3">
+              <label for="username">Username</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">@</span>
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  placeholder="Username"
+                  required=""
+                />
+                <div class="invalid-feedback" style="width: 100%;">
+                  Your username is required.
+                </div>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label for="email"
+                >Email <span class="text-muted">(Optional)</span></label
+              >
+              <input
+                input id="email" 
+                name="email" 
+                type="email" 
+                required="required" 
+                class="form-control"
+                placeholder="you@example.com"
+              />
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label for="address">Address</label>
+              <input
+                id="address" 
+                name="address" 
+                type="text" 
+                required="required" 
+                class="form-control"
+                placeholder="1234 Main St"
+                required=""
+              />
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label for="address2"
+                >Address 2 <span class="text-muted">(Optional)</span></label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="address2"
+                placeholder="Apartment or suite"
+              />
+            </div>
+
+            <div class="row">
+              <div class="col-md-5 mb-3">
+                <label for="country">Country</label>
+                <select
+                  class="custom-select d-block w-100"
+                  id="country"
+                  required=""
+                >
+                  <option value="" selected="selected">Choose...</option>
+                  <option>United States</option>
+                </select>
+                <div class="invalid-feedback">
+                  Please select a valid country.
+                </div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="state">State</label>
+                <select
+                  class="custom-select d-block w-100"
+                  id="state"
+                  required=""
+                >
+                  <option value="" selected="selected">Choose...</option>
+                  <option>California</option>
+                </select>
+                <div class="invalid-feedback">
+                  Please provide a valid state.
+                </div>
+              </div>
+              <div class="col-md-3 mb-3">
+                <label for="zip">Zip</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="zip"
+                  placeholder=""
+                  required=""
+                />
+                <div class="invalid-feedback">
+                  Zip code required.
+                </div>
+              </div>
+            </div>
+            <hr class="mb-4" />
             {{ csrf_field()}}
-            <button type="submit" class="btn btn-success">Buy now</button>
-            
-        </form>
+            <button class="btn btn-primary btn-lg btn-block" type="submit">
+              Continue to checkout
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-</div>
+@endsection
+@section('scripts')
+<script>
+      (function () {
+        "use strict";
+
+        window.addEventListener(
+          "load",
+          function () {
+            var forms = document.getElementsByClassName("needs-validation");
+            var validation = Array.prototype.filter.call(forms, function (
+              form
+            ) {
+              form.addEventListener(
+                "submit",
+                function (event) {
+                  if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                  }
+                  form.classList.add("was-validated");
+                },
+                false
+              );
+            });
+          },
+          false
+        );
+      })();
+    </script>
 @endsection
